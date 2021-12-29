b83875e3422f9da55a5cad6c95c3d71f3d62189a
msla-InspireSA <80754340+msla-InspireSA@users.noreply.github.com>
2021-12-29 23:48:39 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,85 @@
+﻿using Azure;
+using System;
+using Azure.AI.TextAnalytics;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace TextAnalytics_for_health
+{
+    internal class Program
+    {
+
+        //creating connection to azure
+        private static readonly AzureKeyCredential credentials = new AzureKeyCredential("get your own keys");
+        private static readonly Uri endpoint = new Uri("get your own endpoint");
+
+        // Example method for extracting information from healthcare-related text 
+        static async Task healthExample(TextAnalyticsClient client)
+        {
+            string document = "Prescribed 100mg ibuprofen, taken twice daily.";
+
+            List<string> batchInput = new List<string>()
+            {
+                document
+            };
+            AnalyzeHealthcareEntitiesOperation healthOperation = await client.StartAnalyzeHealthcareEntitiesAsync(batchInput);
+            await healthOperation.WaitForCompletionAsync();
+
+            await foreach (AnalyzeHealthcareEntitiesResultCollection documentsInPage in healthOperation.Value)
+            {
+                Console.WriteLine($"Results of Azure Text Analytics for health async model, version: \"{documentsInPage.ModelVersion}\"");
+                Console.WriteLine("");
+
+                foreach (AnalyzeHealthcareEntitiesResult entitiesInDoc in documentsInPage)
+                {
+                    if (!entitiesInDoc.HasError)
+                    {
+                        foreach (var entity in entitiesInDoc.Entities)
+                        {
+                            // view recognized healthcare entities
+                            Console.WriteLine($"  Entity: {entity.Text}");
+                            Console.WriteLine($"  Category: {entity.Category}");
+                            Console.WriteLine($"  Offset: {entity.Offset}");
+                            Console.WriteLine($"  Length: {entity.Length}");
+                            Console.WriteLine($"  NormalizedText: {entity.NormalizedText}");
+                        }
+                        Console.WriteLine($"  Found {entitiesInDoc.EntityRelations.Count} relations in the current document:");
+                        Console.WriteLine("");
+
+                        // view recognized healthcare relations
+                        foreach (HealthcareEntityRelation relations in entitiesInDoc.EntityRelations)
+                        {
+                            Console.WriteLine($"    Relation: {relations.RelationType}");
+                            Console.WriteLine($"    For this relation there are {relations.Roles.Count} roles");
+
+                            // view relation roles
+                            foreach (HealthcareEntityRelationRole role in relations.Roles)
+                            {
+                                Console.WriteLine($"      Role Name: {role.Name}");
+
+                                Console.WriteLine($"      Associated Entity Text: {role.Entity.Text}");
+                                Console.WriteLine($"      Associated Entity Category: {role.Entity.Category}");
+                                Console.WriteLine("");
+                            }
+                            Console.WriteLine("");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("  Error!");
+                        Console.WriteLine($"  Document error code: {entitiesInDoc.Error.ErrorCode}.");
+                        Console.WriteLine($"  Message: {entitiesInDoc.Error.Message}");
+                    }
+                    Console.WriteLine("");
+                }
+            }
+        }
+
+        static async Task Main(string[] args)
+        {
+            var client = new TextAnalyticsClient(endpoint, credentials);
+            await healthExample(client);
+        }
+
+    }
+}
